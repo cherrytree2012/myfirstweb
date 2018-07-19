4dc24c027d35085721598f63fb2bacc85b545c66
cherrytree2012 <798682522@qq.com>
2018-07-19 16:09:40 +0800

    initial commit

---
+++ RazorPagesMovie/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace RazorPagesMovie
+{
+    /// <summary>
+    /// 托管 ASP.NET Core 应用
+    /// 创建一个 web 应用程序宿主
+    /// </summary>
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //初始化web主机
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+    }
+
+    /// <summary>
+    /// WebHost.CreateDefaultBuilder 是在 2.0 中新增的
+    /// </summary>
+    /// <param name="args"></param>
+    /// <returns></returns>
+    //public static IWebHostBuilder CreateDefaultBuilder(string[] args)
+    //{
+    //    var builder = new WebHostBuilder()
+    //        .UseKestrel()//注册 Kestrel 中间件，指定 WebHost 要使用的 Server（HTTP服务器）。
+    //        .UseContentRoot(Directory.GetCurrentDirectory())//设置 Content 根目录，将当前项目的根目录作为 ContentRoot 的目录。
+    //        .ConfigureAppConfiguration((hostingContext, config) =>
+    //        {
+    //读取 appsettinggs.json 配置文件，开发环境下的 UserSecrets 以及环境变量和命令行参数。
+    //            var env = hostingContext.HostingEnvironment;
+
+    //            config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+    //                    .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true);
+
+    //            if (env.IsDevelopment())
+    //            {
+    //                var appAssembly = Assembly.Load(new AssemblyName(env.ApplicationName));
+    //                if (appAssembly != null)
+    //                {
+    //                    config.AddUserSecrets(appAssembly, optional: true);
+    //                }
+    //            }
+
+    //            config.AddEnvironmentVariables();
+
+    //            if (args != null)
+    //            {
+    //                config.AddCommandLine(args);
+    //            }
+    //        })
+    //        .ConfigureLogging((hostingContext, logging) =>
+    //        {
+    //读取配置文件中的 Logging 节点，对日志系统进行配置
+    //            logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+    //            logging.AddConsole();
+    //            logging.AddDebug();
+    //        })
+    //        .UseIISIntegration()//添加 IISIntegration 中间件
+    //        .UseDefaultServiceProvider((context, options) =>
+    //        {
+    //设置开发环境下， ServiceProvider 的 ValidateScopes 为 true，避免直接在 Configure 方法中获取 Scope 实例
+    //            options.ValidateScopes = context.HostingEnvironment.IsDevelopment();
+    //        });
+    //    return builder;
+    //}
+
+
+}
---
+++ RazorPagesMovie/Startup.cs
@@ -0,0 +1,93 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using RazorPagesMovie.Models;
+
+namespace RazorPagesMovie
+{
+    /// <summary>
+    /// 配置服务和请求管道
+    /// </summary>
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        /// <summary>
+        /// 通过依赖注入将服务注册到容器
+        /// 服务是应用中用于通用调用的组件。服务通过依赖注入获取并使用
+        /// </summary>
+        /// <param name="services"></param>
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<IISOptions>(options =>
+            {
+                options.ForwardClientCertificate = false;//禁用将客户端证书转发到应用
+            });
+
+            //向依赖关系注入容器注册数据库上下文
+            services.AddDbContext<MovieContext>
+                (options => options.UseSqlServer(Configuration.GetConnectionString("MovieContext")));
+
+
+            services.AddMvc();//默认将MVC的服务添加到容器中
+        }
+
+        /// <summary>
+        /// 定义请求管道中间件
+        /// ASP.NET Core 中间件为一个 HttpContext 执行异步逻辑，然后按顺序调用下一个中间件或者直接终止请求。
+        /// 一般来说你要使用一个中间件，只需要在 Configure 方法里调用 IApplicationBuilder 上一个对应的 UseXYZ 扩展方法。
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="env"></param>
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+
+            //接收到一个请求时，请求会交给中间件构成的中间件管道进行处理，管道就是多个中间件构成，
+            //请求从一个中间件的一端进入，从中间件的另一端出来，每个中间件都可以对HttpContext请求开始和结束进行处理
+
+
+            //Run()一般在管道尾部被调用；Use可以调用next方法；Map用于分支管道(当前的实现已支持基于请求路径或使用谓词来进入分支)  
+
+            //在开发环境下，显示错误详细信息
+            if (env.IsDevelopment())
+            {
+                app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                //否则，导向错误页面
+                app.UseExceptionHandler("/Error");
+            }
+
+
+            //使用默认起始页(默认根目录是wwwroot)
+            //DefaultFilesOptions options = new DefaultFilesOptions();
+            //options.DefaultFileNames.Clear();
+            //options.DefaultFileNames.Add("index.htm");
+            //app.UseDefaultFiles();
+
+
+
+            //允许访问wwwroot文件夹下的静态文件
+            app.UseStaticFiles();
+
+
+            app.UseMvc();
+        }
+    }
+}
